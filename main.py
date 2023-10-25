628c148b3cea46626e3474a3b5342423d5635de9
zhouzhongping <zhouzhongping@7wate.com>
2023-10-25 17:57:58 +0800

    功能：添加 Flask 及目录调整
     - 添加 Flask 放开公网运行
     - 调整目录符合 Flask 运行

--- main.py
+++ main.py
@@ -2,39 +2,48 @@ from src.analyzer import *
 from src.config import Config
 from src.scraper import Blog
 from loguru import logger
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    return render_template('painting.html', name='World')
+
 
 if __name__ == '__main__':
     # 持久化日志
     # logger.add("endofyear_{time}.log")
-    config = Config("config.ini")
+    # config = Config("config.ini")
+    #
+    # my_blog = Blog(config.rss_url)
+    #
+    # logger.info(f"Blog title: {my_blog.title}")
+    # logger.info(f"Blog link: {my_blog.link}")
+    # logger.info(f"Blog life: {my_blog.life}")
+    # logger.info(f"Total words: {my_blog.article_word_count}")
+    #
+    # for i, post in enumerate(my_blog.post_lists(), 1):
+    #     # 情感分
+    #     post.score = analyze_sentiment(post.content)
+    #     # 分类, 关键字
+    #     post.category, post.keys = classify_and_extract_keywords(text=post.content, topK=7,
+    #                                                              stopwords='data/stop_words.txt',
+    #                                                              tech_terms_file='data/tech_terms.txt')
+    #
+    #     # 权重, 日子计算
+    #     post.weight, post.date = calculate_weight(post.time)
+    #
+    #     # 输出展示
+    #     logger.info(f"Post #{i}:")
+    #     logger.info(f"{type(post.time)}")
+    #     logger.info(post)
+    #     logger.info(f"Keys: {post.score}")
+    #     logger.info(f"Category: {post.category}")
+    #     logger.info(f"Keys: {post.keys}")
+    #     logger.info(f"Weight: {post.weight}")
+    #     logger.info(f"Date: {post.date}")
 
-    my_blog = Blog(config.rss_url)
-
-    logger.info(f"Blog title: {my_blog.title}")
-    logger.info(f"Blog link: {my_blog.link}")
-    logger.info(f"Blog life: {my_blog.life}")
-    logger.info(f"Total words: {my_blog.article_word_count}")
-
-    for i, post in enumerate(my_blog.post_lists(), 1):
-        # 情感分
-        post.score = analyze_sentiment(post.content)
-        # 分类, 关键字
-        post.category, post.keys = classify_and_extract_keywords(text=post.content, topK=7,
-                                                                 stopwords='data/stop_words.txt',
-                                                                 tech_terms_file='data/tech_terms.txt')
-
-        # 权重, 日子计算
-        post.weight, post.date = calculate_weight(post.time)
-
-        # 输出展示
-        logger.info(f"Post #{i}:")
-        logger.info(f"{type(post.time)}")
-        logger.info(post)
-        logger.info(f"Keys: {post.score}")
-        logger.info(f"Category: {post.category}")
-        logger.info(f"Keys: {post.keys}")
-        logger.info(f"Weight: {post.weight}")
-        logger.info(f"Date: {post.date}")
-
-
-
+    # 最后运行
+    app.run(host='0.0.0.0', port=5000, debug=True)
